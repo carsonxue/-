544bf596f467fad1a1bd0d97ec6c09e1d72aa1e4
Carson Xue <carsonxue@Carsons-iMac.local>
2022-04-22 16:20:36 -0500

    port changed

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ app.post("/failure", function(req, res) {
   res.redirect("/contact");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server started on port 3000");
 });
